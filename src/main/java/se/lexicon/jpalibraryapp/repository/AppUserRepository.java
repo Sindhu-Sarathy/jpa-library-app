fceaa3381212fcfc6aefd663014e428f5f16d6ee
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2026-01-14 13:46:12 +0100

    Added Repository for Appuser and userDetails

---
+++ src/main/java/se/lexicon/jpalibraryapp/repository/AppUserRepository.java
@@ -0,0 +1,23 @@
+package se.lexicon.jpalibraryapp.repository;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import se.lexicon.jpalibraryapp.entity.AppUser;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    Optional<AppUser> findByUsername(String username);
+
+    List<AppUser> findByRegDateBetween(LocalDate regDateAfter, LocalDate regDateBefore);
+
+    List<AppUser> findByUserDetails_Id(int userDetailsId);
+
+    @Query("SELECT u FROM AppUser u WHERE LOWER(u.userDetails.email) = LOWER(:email)")
+    Optional<AppUser> findByEmailIgnoreCase(@Param("email") String email);
+}
