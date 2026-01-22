f9c044af2ab67cd1913bf87854292af5ee54dd76
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2026-01-22 10:12:47 +0100

    Created a new branch and added an entity class Book

---
+++ src/main/java/se/lexicon/jpalibraryapp/entity/Book.java
@@ -0,0 +1,34 @@
+package se.lexicon.jpalibraryapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@Entity
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Setter
+    @Column(length = 100,nullable = false)
+    private String isbn;
+
+    @Setter
+    @Column(length = 100,nullable = false)
+    private String title;
+
+    @Setter
+    private int maxLoanDays;
+
+    public Book(String isbn, String title, int maxLoanDays) {
+        this.isbn = isbn;
+        this.title = title;
+        this.maxLoanDays = maxLoanDays;
+    }
+}
